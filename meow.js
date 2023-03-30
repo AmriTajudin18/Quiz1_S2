b8d09f93f434c0c59dcef0278be2692ba4bcaa4b
AmriTajudin18 <amritajudin3636@gmail.com>
2023-03-30 16:03:37 +0800

    upload file meow

---
+++ meow.js
@@ -0,0 +1,54 @@
+let message = "benr"
+console.log(message)
+console.log(typeof message)
+
+
+
+
+
+let str = "1.2322322"
+let num = 1
+
+let total = parseFloat (str) + num
+console.log(total,typeof total)
+
+
+
+
+
+let data = ["benr",4535,true]
+
+console.log(data[0])
+console.log(data[1])
+console.log(data[2])
+console.log(data[3])
+
+
+let user = {
+    name: "meow",
+    age: 19,
+    faculty: "FKEKK"
+}
+console.log(user.name)
+console.log(user.age)
+
+
+let messages = 
+[
+    {
+        name: "mael",
+        date: "28/2/2012",
+        msg: "Hadir boss",
+        like: 500000
+    },
+
+    {
+        name: "alep",
+        date: "13/2/2022",
+        msg: "good video",
+        like: 10000
+
+    },
+]
+
+console.log(messages)
